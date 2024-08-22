b9c8d007a2f583411d9d5b06ab165079f0d1d76a
Deng <garydeng@amd.com>
2024-08-22 11:31:23 -0600

    removed file dir

--- train/comms/pt/pytorch_dist_backend.py
+++ train/comms/pt/pytorch_dist_backend.py
@@ -13,8 +13,8 @@ import numpy as np
 import torch
 import torch.distributed as dist
 import torch.nn as nn
-from param_bench.train.comms.pt.param_profile import paramProfile
-from param_bench.train.comms.pt.pytorch_backend_utils import (
+from param_profile import paramProfile
+from pytorch_backend_utils import (
     backendFunctions,
     collectiveArgsHolder,
 )
